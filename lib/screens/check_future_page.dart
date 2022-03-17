38ec49ce3a1a04a04329b97fd0aa3b3c0b21a1d1
NicholasCG <nicholascgray@knights.ucf.edu>
2022-03-17 18:32:55 -0400

    Some interactivity with calendar

--- lib/screens/check_future_page.dart
+++ lib/screens/check_future_page.dart
@@ -16,11 +16,15 @@ class CheckFuturePage extends StatefulWidget {
 }
 
 class _CheckFuturePageState extends State<CheckFuturePage> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime _selectedDay;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Garage Predictor"),
+        title: Text("Check Future Page"),
         backgroundColor: kPrimaryColor,
         centerTitle: true,
         // leading: IconButton(
@@ -50,25 +54,42 @@ class _CheckFuturePageState extends State<CheckFuturePage> {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(15.0),
-            child: Text(
-              'Check Future Page',
-              // style: kTitleTextStyle,
-            ),
-          ),
           TableCalendar(
             firstDay: DateTime.utc(2010, 10, 16),
             lastDay: DateTime.utc(2030, 3, 14),
-            focusedDay: DateTime.now(),
+            focusedDay: _focusedDay,
+            calendarFormat: _calendarFormat,
+            selectedDayPredicate: (day) {
+              // Use `selectedDayPredicate` to determine which day is currently selected.
+              // If this returns true, then `day` will be marked as selected.
+
+              // Using `isSameDay` is recommended to disregard
+              // the time-part of compared DateTime objects.
+              return isSameDay(_selectedDay, day);
+            },
+            onDaySelected: (selectedDay, focusedDay) {
+              if (!isSameDay(_selectedDay, selectedDay)) {
+                // Call `setState()` when updating the selected day
+                setState(() {
+                  _selectedDay = selectedDay;
+                  _focusedDay = focusedDay;
+                });
+              }
+            },
+            onFormatChanged: (format) {
+              if (_calendarFormat != format) {
+                // Call `setState()` when updating calendar format
+                setState(() {
+                  _calendarFormat = format;
+                });
+              }
+            },
+            onPageChanged: (focusedDay) {
+              // No need to call `setState()` here
+              _focusedDay = focusedDay;
+            },
           ),
-          // Container(
-          //     padding: EdgeInsets.all(15.0),
-          //     child: Text(
-          //       'A calendar will appear here!',
-          //       textAlign: TextAlign.center,
-          //       // style: kTitleTextStyle,
-          //     )),
+
           ElevatedButton(
             style: ElevatedButton.styleFrom(
               primary: kPrimaryColor,

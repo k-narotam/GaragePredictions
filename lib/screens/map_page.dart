3704246f1e55f2965176a43e7e4c12a2f3066430
Ben Bowles <benjaminallenbowles@gmail.com>
2022-03-17 20:09:56 -0400

    Map
    Implemented a simple version of the map.

--- lib/screens/map_page.dart
+++ lib/screens/map_page.dart
@@ -1,45 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import "package:latlong2/latlong.dart" as latLng;
+import 'package:theme_manager/screens/nav_drawer.dart';
 import '../constants.dart';
 
 class MapPage extends StatelessWidget {
+  FlutterMap myMap;
+  TileLayerOptions myOptions;
   MapPage();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Garage Predictor'),
-        backgroundColor: kPrimaryColor,
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(15.0),
-            child: Text(
-              'Map Page',
-              // style: kTitleTextStyle,
-            ),
+        appBar: AppBar(
+          title: Text('Garage Predictor'),
+          backgroundColor: kPrimaryColor,
+        ),
+        drawer: NavDrawer(),
+        body: new FlutterMap(
+          options: MapOptions(
+            center: latLng.LatLng(28.6, -81.2),
+            zoom: 15.0,
           ),
-          Container(
-              padding: EdgeInsets.all(15.0),
-              child: Text(
-                'There will be a map here! Ooh!',
-                textAlign: TextAlign.center,
-                // style: kTitleTextStyle,
-              )),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              primary: kPrimaryColor,
+          layers: [
+            TileLayerOptions(
+              urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+              subdomains: ['a', 'b', 'c'],
+              attributionBuilder: (_) {
+                return Text("");
+              },
             ),
-            onPressed: () {
-              //Navigator.pushNamed(context, '/');
-              Navigator.pop(context);
-            },
-            child: Text('Close Page'),
-          ),
-        ],
-      ),
-    );
+            MarkerLayerOptions(
+              markers: [
+                Marker(
+                  width: 80.0,
+                  height: 80.0,
+                  point: latLng.LatLng(51.5, -0.09),
+                  builder: (ctx) => Container(
+                    child: FlutterLogo(),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ));
   }
 }

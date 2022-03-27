932c9a26fb6485d3784bd231bb8101db9818cf4a
NicholasCG <nicholascgray@knights.ucf.edu>
2022-03-26 20:54:40 -0400

    Icon fix and garages marked

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,15 +1,14 @@
 import { MapContainer, useMap, useMapEvent, Rectangle,  TileLayer, Marker, Tooltip, Popup } from "react-leaflet";
-import { LatLngExpression } from "leaflet";
+import { LatLngExpression, Icon} from "leaflet";
+import markerIconPng from "leaflet/dist/images/marker-icon.png"
 import "leaflet/dist/leaflet.css";
 import Navbar from "../components/Navbar";
 import React from 'react';
 
-
-
 const Map = () => {
   
   const defaultPosition = [28.6016, -81.2005]; // UCF position
-
+  const customIcon = new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})
   return (
     
     <div className="map__container">
@@ -29,9 +28,63 @@ const Map = () => {
         />
 
         
-        <Marker position={[28.600003615431984, -81.20547129871996]}>
+        <Marker position={[28.600003615431984, -81.20547129871996]} icon={customIcon}>
             <Popup>
-                I am a pop-up!
+                Garage A
+            </Popup>
+        </Marker>
+
+        <Marker position={[28.596890438568995, -81.20035892832448]} icon={customIcon}>
+            <Popup>
+                Garage B
+            </Popup>
+        </Marker>
+
+        <Marker position={[28.602277201961225, -81.19592179707529]} icon={customIcon}>
+            <Popup>
+                Garage C
+            </Popup>
+        </Marker>
+
+        <Marker position={[28.60492516940185, -81.19719043221895]} icon={customIcon}>
+            <Popup>
+                Garage D
+            </Popup>
+        </Marker>
+
+        <Marker position={[28.606502435914777, -81.19526413069644]} icon={customIcon}>
+            <Popup>
+                Garage E
+            </Popup>
+        </Marker>
+
+        <Marker position={[28.608563174014833, -81.19654445789656]} icon={customIcon}>
+            <Popup>
+                Garage F
+            </Popup>
+        </Marker>
+
+        <Marker position={[28.608831817390655, -81.19774450482778]} icon={customIcon}>
+            <Popup>
+                Garage G
+            </Popup>
+        </Marker>
+
+        <Marker position={[28.604947958016297, -81.20115972129959]} icon={customIcon}>
+            <Popup>
+                Garage H
+            </Popup>
+        </Marker>
+
+        <Marker position={[28.60111379729534, -81.20479218080769]} icon={customIcon}>
+            <Popup>
+                Garage I
+            </Popup>
+        </Marker>
+
+        <Marker position={[28.596149388993762, -81.19672787522661]} icon={customIcon}>
+            <Popup>
+                Libra Garage
             </Popup>
         </Marker>
       </MapContainer>

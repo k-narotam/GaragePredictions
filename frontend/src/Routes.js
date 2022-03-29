38c25c0d330c96bb4a1d72a25db1d1be9655c849
Pedro Fachetti Carvalho <pfachetti.pc@gmail.com>
2022-03-29 15:12:29 -0400

    new branch clean-up

--- frontend/src/Routes.js
+++
@@ -1,6 +0,0 @@
-import Login from "./containers/Login";
-<Route exact path="/login">
-
-  <Login />
-
-</Route>
--- frontend/src/components/CurrentGarageStats.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-export default function CurrentGarageStats()  {
-  
-    return <iframe src="https://secure.parking.ucf.edu/GarageCount/" width = "100%" height = "500"/>;
-  
-}

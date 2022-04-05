8efa97d669c3aebeb10f0046aa6357874698ea12
NicholasCG <nicholascgray@knights.ucf.edu>
2022-04-05 19:04:23 -0400

    Changing Imports

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from "react";
 import Link from '@mui/material/Link';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
--- src/components/TrendsButton.js
+++ src/components/TrendsButton.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import Link from '@mui/material/Link';
 import Title from './Title';
 

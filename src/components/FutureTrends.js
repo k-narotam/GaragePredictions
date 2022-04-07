8527e5acb6c2be90c822531b5a4f7da2d9c41010
NicholasCG <nicholascgray@knights.ucf.edu>
2022-04-07 18:01:50 -0400

    Revert "Revert "Beginning of Trends Tab""
    This reverts commit 214b747e6b0b6be35e6a5fe2976aca47c1ff3906.

--- src/components/FutureTrends.js
+++ src/components/FutureTrends.js
@@ -1,33 +1,84 @@
 import React, { useState } from "react";
-// import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
-import DateTimePicker from 'react-datetime-picker'
+import Typography from '@mui/material/Typography';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import FormControl from '@mui/material/FormControl';
+import Grid from '@mui/material/Grid';
 
 
-export default function FutureTrends()  {
+export default function FutureTrends() {
 
-    const [value, onChange] = useState(new Date());
+  const [weekday, setWeekday] = useState('');
+  const [garage, setGarage] = useState('');
+
+  const handleWeekdayChange = (event) => {
+    setWeekday(event.target.value);
+  };
+
+  const handleGarageChange = (event) => {
+    setGarage(event.target.value);
+  };
 
   return (
     <div>
-      <DateTimePicker onChange={onChange} value={value} />
-     
+      <Grid 
+        container 
+        spacing={2} 
+        direction="row" 
+        justifyContent={"center"} 
+        alignItems={"center"}
+        mt={5}
+        >
+        <Grid item>
+          <Typography variant="h5">Check next</Typography>
+        </Grid>
+
+        <Grid item>
+          <FormControl sx={{ m: 1, minWidth: 120 }}>
+            <InputLabel id="future-weekday">Weekday</InputLabel>
+            <Select
+              labelId="future-weekday"
+              id="future-weekday-select"
+              value={weekday}
+              label="Weekday"
+              onChange={handleWeekdayChange}
+            >
+              <MenuItem value={0}>Sunday</MenuItem>
+              <MenuItem value={24}>Monday</MenuItem>
+              <MenuItem value={48}>Tuesday</MenuItem>
+              <MenuItem value={72}>Wednesday</MenuItem>
+              <MenuItem value={96}>Thursday</MenuItem>
+              <MenuItem value={120}>Friday</MenuItem>
+              <MenuItem value={144}>Saturday</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+
+        <Grid item>
+          <Typography variant="h5">at Garage</Typography>
+        </Grid>
+
+        <Grid item>
+
+          <FormControl sx={{ m: 1, minWidth: 120 }}>
+            <InputLabel id="future-garage">Garage</InputLabel>
+            <Select
+              labelId="future-garage"
+              id="future-garage-select"
+              value={garage}
+              label="Garage"
+              onChange={handleGarageChange}
+            >
+              <MenuItem value={'a'}>A</MenuItem>
+              <MenuItem value={'c'}>C</MenuItem>
+              <MenuItem value={'d'}>D</MenuItem>
+              <MenuItem value={'i'}>I</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+      </Grid>
     </div>
-    
   );
-
-    
-    
-
-    /*
-  
-    const [date, setDate] = useState(new Date());
-    return (
-        <div>
-            <Calendar onChange={(date) => setDate(date)} value={date} />
-        </div>
-    );
-
-    */
-  
 }
+

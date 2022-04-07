ca3e08aafb926cec087a379c0e527bc05f2127c9
NicholasCG <nicholascgray@knights.ucf.edu>
2022-04-07 17:38:54 -0400

    Revert "Dummy trends page"
    This reverts commit 2dae4fa007c4eea68a7bf5f54bbb9351779b3cab.

--- src/components/FutureTrends.js
+++ src/components/FutureTrends.js
@@ -6,106 +6,6 @@ import Select from '@mui/material/Select';
 import FormControl from '@mui/material/FormControl';
 import Grid from '@mui/material/Grid';
 
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
-const data = [
-  {
-    name: '12 AM',
-    filled: 0,
-  },
-  {
-    name: '1 AM',
-    filled: 0,
-  },
-  {
-    name: '2 AM',
-    filled: 0,
-  },
-  {
-    name: '3 AM',
-    filled: 0,
-  },
-  {
-    name: '4 AM',
-    filled: 0,
-  },
-  {
-    name: '5 AM',
-    filled: 0,
-  },
-  {
-    name: '6 AM',
-    filled: 5,
-  },
-  {
-    name: '7 AM',
-    filled: 10,
-  },
-  {
-    name: '8 AM',
-    filled: 20,
-  },
-  {
-    name: '9 AM',
-    filled: 50,
-  },
-  {
-    name: '10 AM',
-    filled: 70,
-  },
-  {
-    name: '11 AM',
-    filled: 90,
-  },
-  {
-    name: '12 PM',
-    filled: 100,
-  },
-  {
-    name: '1 PM',
-    filled: 90,
-  },
-  {
-    name: '2 PM',
-    filled: 70,
-  },
-  {
-    name: '3 PM',
-    filled: 60,
-  },
-  {
-    name: '4 PM',
-    filled: 30,
-  },
-  {
-    name: '5 PM',
-    filled: 30,
-  },
-  {
-    name: '6 PM',
-    filled: 20,
-  },
-  {
-    name: '7 PM',
-    filled: 15,
-  },
-  {
-    name: '8 PM',
-    filled: 10,
-  },
-  {
-    name: '9 PM',
-    filled: 5,
-  },
-  {
-    name: '10 PM',
-    filled: 0,
-  },
-  {
-    name: '11 PM',
-    filled: 0,
-  },
-];
 
 export default function FutureTrends() {
 
@@ -120,54 +20,16 @@ export default function FutureTrends() {
     setGarage(event.target.value);
   };
 
-  const handleGraph = () => {
-    console.log(weekday);
-    if (weekday === '' && garage === '') {
-      return (
-        <div>
-          <Typography variant="h6" color="#c79632" fontWeight= "bold" gutterBottom>
-            Select a weekday and a garage to see the future trends.
-          </Typography>
-        </div>
-      );
-    }
-    else if (weekday === 24 && garage === 'c') {
-      return (
-        <ResponsiveContainer width={"75%"} aspect={2.25}>
-          <LineChart
-            width={500}
-            height={300}
-            data={data}
-            margin={{
-              top: 5,
-              right: 30,
-              left: 20,
-              bottom: 5,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Line type="monotone" dataKey="filled" stroke="#8884d8" activeDot={{ r: 8 }} />
-          </LineChart>
-        </ResponsiveContainer>
-      );
-    }
-    else {
-      return ("No data available");
-    }
-  };
   return (
     <div>
-      <Grid
-        container
-        spacing={2}
-        direction="row"
-        justifyContent={"center"}
+      <Grid 
+        container 
+        spacing={2} 
+        direction="row" 
+        justifyContent={"center"} 
         alignItems={"center"}
         mt={5}
-      >
+        >
         <Grid item>
           <Typography variant="h5">Check next</Typography>
         </Grid>
@@ -216,11 +78,6 @@ export default function FutureTrends() {
           </FormControl>
         </Grid>
       </Grid>
-
-      <Grid item >
-        {handleGraph()}
-      </Grid>
-
     </div>
   );
 }

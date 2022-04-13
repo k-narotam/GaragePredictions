95e1b51b104d5539193abc6607ae6bfbd19a4203
Nicholas Gray <70730217+NicholasCG@users.noreply.github.com>
2022-04-13 17:27:34 -0400

    Update constants.py

--- api/constants.py
+++ api/constants.py
@@ -25,7 +25,7 @@ garage_pos_map = {
     'l': 6,
 }
 
-origins = ["localhost:3000", "https://ucfgaragepredictions.herokuapp.com"]
+origins = ["http://localhost:3000", "https://ucfgaragepredictions.herokuapp.com"]
 
 def garage_pos(garage_id):
     return garage_pos_map[garage_id]

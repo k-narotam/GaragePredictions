bd1396f646282353be07fd90d6ffdb0a729c84ac
NicholasCG <nicholascgray@knights.ucf.edu>
2022-04-17 18:33:10 -0400

    Fixed unit tests

--- integration_tests.py
+++ integration_tests.py
@@ -4,12 +4,25 @@ import pytest
 from flask import session
 
 from app import app
+from api.structures import User
+from api.database import db
+import bcrypt
 
 @pytest.fixture()
 def client():
     return app.test_client()
 
 def test_login_valid(client):
+
+   # Make test verified test account
+    password = ('qwertypass').encode('utf-8')
+    email = 'qwerty@gmail.com'
+    salt = bcrypt.gensalt()
+    password_hash = bcrypt.hashpw(password, salt)
+    new_user = User().create(password_hash, salt, email)
+    new_user.confirmed = True
+    new_user.save()
+
     r = client.post('/login', json={'email': 'qwerty@gmail.com', 'password': 'qwertypass'})
     assert r.status_code == 200
     assert isinstance(r.json, dict)
@@ -25,21 +38,30 @@ def test_login_invalid_pass(client):
     r = client.post('/login', json={'email': 'qwerty@gmail.com', 'password': 'qwertypasss'})
     assert r.status_code == 200
     assert isinstance(r.json, dict)
-    assert r.json['error'] == 'user does not exist'
+    assert r.json['error'] == 'Invalid Username/Password'
 
 def test_register_invalid_email(client):
     r = client.post('/register', json={'email': 'qwerty@gmail.com', 'password': 'newpass'})
     assert r.status_code == 200
     assert isinstance(r.json, dict)
-    assert r.json['error'] == 'user already exists'
+    assert r.json['error'] == 'User Already Exists'
 
 def test_register(client):
+    db['users'].delete_one({'_id': 'itestacc@gmail.com'})
+    
     r = client.post('/register', json={'email': 'itestacc@gmail.com', 'password': 'newpass'})
+
     assert r.status_code == 200
     assert isinstance(r.json, dict)
     assert r.json['error'] == ''
 
 def test_delete_acc(client):
+
+    # Confirm test account
+    user = User().load('itestacc@gmail.com')
+    user.confirmed = True
+    user.save()
+
     r = client.post('/login', json={'email': 'itestacc@gmail.com', 'password': 'newpass'})
     r2 = client.post('/delete_acc')
     assert r.status_code == 200
@@ -126,14 +148,14 @@ def test_trend_invalid(client):
 
 def test_favorites(client):
     r = client.post('/login', json={'email': 'qwerty@gmail.com', 'password': 'qwertypass'})
-    r2 = client.post('/list_favorites', json={'garage_id': ''})
+    r2 = client.post('/list_favorites', json={'garage_id': ""})
     r3 = client.post('/add_favorite', json={'garage_id': 'i', 'weekday': 'tue', 'time': 2.2})
     r4 = client.post('/add_favorite', json={'garage_id': 'i', 'weekday': 'tue', 'time': 3.2})
     r10 = client.post('/add_favorite', json={'garage_id': 'i', 'weekday': 'tue', 'time': 3.2})
-    r5 = client.post('/list_favorites', json={'garage_id': ''})
+    r5 = client.post('/list_favorites', json={'garage_id': ""})
     r6 = client.post('/delete_favorite', json={'garage_id': 'i', 'weekday': 'tue', 'time': 2.2})
     r7 = client.post('/delete_favorite', json={'garage_id': 'i', 'weekday': 'tue', 'time': 3.2})
-    r8 = client.get('/list_favorites', json={'garage_id': ''})
+    r8 = client.post('/list_favorites', json={'garage_id': ""})
     r9 = client.post('/delete_favorite', json={'garage_id': 'i', 'weekday': 'tue', 'time': 3.2})
 
     assert r.status_code == 200
@@ -169,7 +191,7 @@ def test_favorites(client):
     assert r10.json['error'] == 'favorite prediction already exists'
 
 def test_favorites_unauth(client):
-    r = client.get('/list_favorites')
+    r = client.post('/list_favorites', json={'garage_id': ''})
     r2 = client.post('/add_favorite', json={'garage_id': 'i', 'weekday': 'tue', 'time': 2.2})
     r3 = client.post('/delete_favorite', json={'garage_id': 'i', 'weekday': 'tue', 'time': 2.2})
 
